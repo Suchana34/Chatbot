09c35706baf0df22494e6d1202d2e3b59ebe910a
suchana <suchanachakraborty@gmail.com>
2020-01-24 11:06:36 +0530

    Added dictionary based chitchat

---
+++ chitchat.py
@@ -0,0 +1,30 @@
+name = "Greg"
+weather = "cloudy"
+
+# Defining a dictionary with the predefined responses
+responses = {
+  "what's your name?": "my name is {0}".format(name),
+  "what's today's weather?": "the weather is {0}".format(weather),
+  "default": "default message"
+}
+
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        bot_message = responses[message]
+    else:
+        bot_message = responses["default"]
+    return bot_message
+
+bot_template = "BOT :{0}"
+user_template = "USER : {0}"
+
+def send_message(message):
+    print(user_template.format(message))
+
+    response = respond(message)
+
+    print(bot_template.format(response))
+
+send_message("what's today's weather?")
+send_message("what's your name?")
