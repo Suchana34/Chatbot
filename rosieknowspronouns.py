b2e88641e7fbe9b4909ae87335ca37d2d4bb20e3
suchana <suchanachakraborty@gmail.com>
2020-01-24 12:44:02 +0530

    rosie learns pronouns

---
+++ rosieknowspronouns.py
@@ -0,0 +1,47 @@
+from rosiechatbot import match_rule, rules
+import re
+
+def replace_pronouns(message):
+
+    message = message.lower()
+    if 'me' in message:
+        # Replace 'me' with 'you'
+        return re.sub('me', 'you', message)
+    if 'my' in message:
+        # Replace 'my' with 'your'
+        return re.sub('my', 'your', message)
+    if 'your' in message:
+        # Replace 'your' with 'my'
+        return re.sub('your', 'my', message)
+    if 'you' in message:
+        # Replace 'you' with 'me'
+        return re.sub('you', 'me', message)
+
+    return message
+
+def respond(message):
+    phrase = match_rule(rules, message)
+    response = match_rule(rules, message)
+    
+    if '{0}' in response:
+        # Replace the pronouns in the phrase
+        phrase = replace_pronouns(phrase)
+        # Include the phrase in the response
+        response = response.format(phrase)
+    return response
+
+bot_template = "BOT :{0}"
+user_template = "USER : {0}"
+
+def send_message(question):
+    print(user_template.format(question))
+
+    response = respond(question)
+
+    print(bot_template.format(response))
+
+
+send_message("do you remember your last birthday")
+send_message("do you think humans should be worried about AI")
+send_message("I want a robot friend")
+send_message("what if you could be anything you wanted")
